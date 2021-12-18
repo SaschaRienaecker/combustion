fb78747badce7550b692540083c336c7c28b14c2
slimguat <mzerguat.sl@gmail.com>
2021-12-18 12:47:54 +0100

    ;

--- charac_time.py
+++ charac_time.py
@@ -7,6 +7,10 @@ from fluid_flow import set_boundary,compute_P,df1_2
 from numba import jit
 import pickle
 from parameters import *
+from matplotlib.animation import FuncAnimation
+import matplotlib.animation as animation
+
+
 u0 = np.zeros((N,M))
 v0 = np.copy(u0)
 u0,v0 = set_boundary(u0,v0)
@@ -71,41 +75,28 @@ def advance_fluid_flow(st_ratio:float, u, v, f, dt):
             U=new_U.copy()
             V=new_V.copy()
         
-        if inc_N%10000==0: #plot results every 10000 
-            # fig, ax = plt.subplots(figsize=(16,9))
-            
-            # ax.set_title('velocity field')
-            # ax.set_xlabel('$x$')
-            # ax.set_ylabel('$y$')
-
-            # im =ax.imshow(
-            #     (np.nan_to_num(
-            #         np.abs(u-U[inc_N-1])*2/(np.abs(U[inc_N-1]+u))
-            #         ,nan=0)+
-            #      np.nan_to_num(
-            #         np.abs(v-V[inc_N-1])*2/(np.abs(V[inc_N-1]+v))
-            #     ,nan=0)).T
-            #     )
+        if False:#inc_N%10000==0: #plot results every 10000 
+            fig, ax = plt.subplots()
             _,std_U = stdclose(U,inc_N,return_val=True)
             _,std_V = stdclose(V,inc_N,return_val=True)
             print(np.max(std_U) , np.max(std_V))
-            # im =ax.imshow(
-            #     (std_U+std_V).T
-            #     )
-            # ax.invert_yaxis()
-            # ax.quiver(U[inc_N-1].T, V[inc_N-1].T)
-            # fig.colorbar(im, ax=ax)
-            # plt.show()
-            # plt.plot(np.mean(np.nan_to_num(
-            #     np.abs((U[0:-1]-U[1:]))*2/np.abs(U[0:-1]+U[1:])
-            #     ,nan=0),axis=(1,2))
-            #     +
-            #     np.mean(np.nan_to_num(
-            #     np.abs((V[0:-1]-V[1:]))*2/np.abs(V[0:-1]+V[1:])
-            #     ,nan=0),axis=(1,2)
-            #     ))
-            # plt.yscale('log')
-            # plt.show()
+            im =ax.imshow(
+                (std_U+std_V).T
+                )
+            ax.invert_yaxis()
+            ax.quiver(U[inc_N-1].T, V[inc_N-1].T)
+            fig.colorbar(im, ax=ax)
+            plt.show()
+            plt.plot(np.mean(np.nan_to_num(
+                np.abs((U[0:-1]-U[1:]))*2/np.abs(U[0:-1]+U[1:])
+                ,nan=0),axis=(1,2))
+                +
+                np.mean(np.nan_to_num(
+                np.abs((V[0:-1]-V[1:]))*2/np.abs(V[0:-1]+V[1:])
+                ,nan=0),axis=(1,2)
+                ))
+            plt.yscale('log')
+            plt.show()
         U[inc_N]=u.copy()
         V[inc_N]=v.copy()
         #this technique could be wrong because it's seen that the results fluctuate so it's better to see the standard deviation
@@ -115,17 +106,42 @@ def advance_fluid_flow(st_ratio:float, u, v, f, dt):
 
         #checking the stationarity by std over an extend of time and see if the overale variation is steady
         std_extend = 100
-        stationary = stdclose(U,inc_N,std_extend=std_extend) and stdclose(V,inc_N,std_extend=std_extend) if inc_N>std_extend else False
+        stationary = stdclose(U,inc_N,std_extend=std_extend,rtot=0.01) and stdclose(V,inc_N,std_extend=std_extend,rtot=0.01) if inc_N>std_extend else False
 
         print(inc_N) if inc_N%1000==0 else 0
         inc_N+=1
     return U[:inc_N], V[:inc_N]
-U, V = advance_fluid_flow(0.001, u, v, advance_adv_diff_RK3, dt)
-pickle.dump([U,V],open("U_V_field-std_0.01.p",'wb'))
-fig, ax = plt.subplots(figsize=(4,4))
-ax.quiver(U[-1].T, V[-1].T)
-ax.set_title('velocity field')
-ax.set_xlabel('$x$')
-ax.set_ylabel('$y$')
-plt.show()
-print(U.shape)
+
+if False: #generating stationary velocity field
+    U, V = advance_fluid_flow(0.001, u, v, advance_adv_diff_RK3, dt)
+    pickle.dump([U,V],open("U_V_field-std_0.01.p",'wb'))
+    fig, ax = plt.subplots(figsize=(4,4))
+    ax.quiver(U[-1].T, V[-1].T)
+    ax.set_title('velocity field')
+    ax.set_xlabel('$x$')
+    ax.set_ylabel('$y$')
+    plt.show()
+    print(U.shape)
+
+#animation comes after
+def update(frame):
+    ln.set_UVC(U[frame].T, V[frame].T)
+    # ln[1].set_data([V[frame]**2+U[frame]**2])
+    return ln,
+
+
+if True: #Analysing the results and generating video
+    #saving a video
+    U,V = pickle.load(open("U_V_field-std_0.01.p","rb"))
+    fig, ax = plt.subplots()
+    ln1 = ax.quiver(U[0].T, V[0].T,scale=40)
+
+    # ln2 = ax.imshow(U[0]**2+V[0]**2)
+    ln = ln1#[ln1,ln2]
+
+    plt.rcParams['animation.ffmpeg_path'] = r'C:\Users\hp\Documents\ffmpeg-2021-10-18-git-d04c005021-full_build\bin/ffmpeg.exe'
+    writer = animation.writers['ffmpeg']
+    writer = writer(fps=60, metadata=dict(artist='Slimane MZERGUAT'), bitrate=1800)
+    ani = FuncAnimation(fig, update, blit=True, frames=10000,interval=0.1,repeat=False)
+    ani.save("res.mp4", writer=writer)
+    plt.show()
