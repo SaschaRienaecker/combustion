e84fdb494dfdc0e3058259f8552f01c036388f32
slimguat <mzerguat.sl@gmail.com>
2021-12-18 01:28:43 +0100

    correcting some errors

--- charac_time.py
+++ charac_time.py
@@ -5,6 +5,7 @@ import numpy as np
 from Funcs import advance_adv_diff_RK3
 from fluid_flow import set_boundary,compute_P,df1_2
 from numba import jit
+import pickle
 from parameters import *
 u0 = np.zeros((N,M))
 v0 = np.copy(u0)
@@ -13,13 +14,13 @@ u0,v0 = set_boundary(u0,v0)
 u, v = np.copy(u0), np.copy(v0)
 dt = 1e-7
 # @jit(nopython=True)
-def stdclose(A,N,rtot=0.1,std_extend=100,return_val = False, axis= 0):
+def stdclose(A,N,rtot=0.01,std_extend=100,return_val = False, axis= 0):
     res = np.std(A[N-std_extend :N],axis = axis)/np.abs(np.mean(A[N-std_extend :N],axis=axis))
     res = np.nan_to_num(res,nan=0)
     if return_val:
-        return np.all(res==rtot),res
+        return np.all(res<=rtot),res
     else:
-        return np.all(res==rtot)
+        return np.all(res<=rtot)
 
 @jit(nopython=True)
 def allclose(a,b,rtot=10**-5,atot=0):
@@ -71,11 +72,12 @@ def advance_fluid_flow(st_ratio:float, u, v, f, dt):
             V=new_V.copy()
         
         if inc_N%10000==0: #plot results every 10000 
-            fig, ax = plt.subplots(figsize=(16,9))
+            # fig, ax = plt.subplots(figsize=(16,9))
             
-            ax.set_title('velocity field')
-            ax.set_xlabel('$x$')
-            ax.set_ylabel('$y$')
+            # ax.set_title('velocity field')
+            # ax.set_xlabel('$x$')
+            # ax.set_ylabel('$y$')
+
             # im =ax.imshow(
             #     (np.nan_to_num(
             #         np.abs(u-U[inc_N-1])*2/(np.abs(U[inc_N-1]+u))
@@ -86,24 +88,24 @@ def advance_fluid_flow(st_ratio:float, u, v, f, dt):
             #     )
             _,std_U = stdclose(U,inc_N,return_val=True)
             _,std_V = stdclose(V,inc_N,return_val=True)
-            
-            im =ax.imshow(
-                (std_U+std_V).T
-                )
-            ax.invert_yaxis()
-            ax.quiver(U[inc_N-1].T, V[inc_N-1].T)
-            fig.colorbar(im, ax=ax)
-            plt.show()
-            plt.plot(np.mean(np.nan_to_num(
-                np.abs((U[0:-1]-U[1:]))*2/np.abs(U[0:-1]+U[1:])
-                ,nan=0),axis=(1,2))
-                +
-                np.mean(np.nan_to_num(
-                np.abs((V[0:-1]-V[1:]))*2/np.abs(V[0:-1]+V[1:])
-                ,nan=0),axis=(1,2)
-                ))
-            plt.yscale('log')
-            plt.show()
+            print(np.max(std_U) , np.max(std_V))
+            # im =ax.imshow(
+            #     (std_U+std_V).T
+            #     )
+            # ax.invert_yaxis()
+            # ax.quiver(U[inc_N-1].T, V[inc_N-1].T)
+            # fig.colorbar(im, ax=ax)
+            # plt.show()
+            # plt.plot(np.mean(np.nan_to_num(
+            #     np.abs((U[0:-1]-U[1:]))*2/np.abs(U[0:-1]+U[1:])
+            #     ,nan=0),axis=(1,2))
+            #     +
+            #     np.mean(np.nan_to_num(
+            #     np.abs((V[0:-1]-V[1:]))*2/np.abs(V[0:-1]+V[1:])
+            #     ,nan=0),axis=(1,2)
+            #     ))
+            # plt.yscale('log')
+            # plt.show()
         U[inc_N]=u.copy()
         V[inc_N]=v.copy()
         #this technique could be wrong because it's seen that the results fluctuate so it's better to see the standard deviation
@@ -113,14 +115,13 @@ def advance_fluid_flow(st_ratio:float, u, v, f, dt):
 
         #checking the stationarity by std over an extend of time and see if the overale variation is steady
         std_extend = 100
-        stationary = stdclose(U,inc_N) and stdclose(V,inc_N) if inc_N>100 else False
+        stationary = stdclose(U,inc_N,std_extend=std_extend) and stdclose(V,inc_N,std_extend=std_extend) if inc_N>std_extend else False
 
         print(inc_N) if inc_N%1000==0 else 0
         inc_N+=1
     return U[:inc_N], V[:inc_N]
-        
 U, V = advance_fluid_flow(0.001, u, v, advance_adv_diff_RK3, dt)
-
+pickle.dump([U,V],open("U_V_field-std_0.01.p",'wb'))
 fig, ax = plt.subplots(figsize=(4,4))
 ax.quiver(U[-1].T, V[-1].T)
 ax.set_title('velocity field')
